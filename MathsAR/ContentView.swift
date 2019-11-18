0bc168e5ae195fe4ad937bfc139be75db2a56573
Odhran Daly <odhran.daly25@mail.dcu.ie>
2019-11-18 23:26:44 +0000

    #7 Create tab bar for three screens

--- MathsAR/ContentView.swift
+++ MathsAR/ContentView.swift
@@ -10,12 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        AppTabbedView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppTabbedView: View {
+    init() {
+        UITabBar.appearance().barTintColor = .white
+        UITabBar.appearance().backgroundColor = .white
+        UITabBar.appearance().layer.borderWidth = 0
+        UITabBar.appearance().layer.borderColor = UIColor.black.cgColor
+        UITabBar.appearance().clipsToBounds = true
+    }
+    var body: some View {
+        TabView() {
+            Text("First View")
+                .tabItem {
+                    Image(systemName: "1.circle")
+                    Text("First")
+                }.tag(0)
+            Text("Second View")
+                .tabItem {
+                    Image(systemName: "2.circle")
+                    Text("Second")
+                }.tag(1)
+            Text("Third View")
+                .tabItem {
+                    Image(systemName: "3.circle")
+                    Text("Third")
+                }.tag(2)
+            }.accentColor(.black)
     }
 }
